b8e8f014c5f2dbaa03d4ceb1935174b85fc9430d
Chuan-Yen Chiang <cychiang0823@gmail.com>
2023-10-27 19:24:44 +0200

    update crds, and configs

--- config/compute/config.go
+++ config/compute/config.go
@@ -6,5 +6,6 @@ import ujconfig "github.com/upbound/upjet/pkg/config"
 func Configure(p *ujconfig.Provider) {
 	p.AddResourceConfigurator("kraftcloud_instance", func(r *ujconfig.Resource) {
 		r.Kind = "Instance"
+		r.ShortGroup = "compute"
 	})
 }
---
+++ internal/controller/zz_setup.go
@@ -0,0 +1,28 @@
+/*
+Copyright 2021 Upbound Inc.
+*/
+
+package controller
+
+import (
+	ctrl "sigs.k8s.io/controller-runtime"
+
+	"github.com/upbound/upjet/pkg/controller"
+
+	instance "github.com/cychiang/provider-kraftcloud/internal/controller/compute/instance"
+	providerconfig "github.com/cychiang/provider-kraftcloud/internal/controller/providerconfig"
+)
+
+// Setup creates all controllers with the supplied logger and adds them to
+// the supplied manager.
+func Setup(mgr ctrl.Manager, o controller.Options) error {
+	for _, setup := range []func(ctrl.Manager, controller.Options) error{
+		instance.Setup,
+		providerconfig.Setup,
+	} {
+		if err := setup(mgr, o); err != nil {
+			return err
+		}
+	}
+	return nil
+}
